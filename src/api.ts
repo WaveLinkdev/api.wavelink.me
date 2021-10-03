b74f97af0a2c69e8e7104f00a987a830787d2022
WaveLink <wavelink@email.cz>
2021-10-03 21:10:59 +0200

    cscs

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,5 @@
 import express from "express";
+const { Webhook } = require("discord-webhook-node");
 
 export class API {
     app: express.Application;
@@ -14,15 +15,25 @@ export class API {
         this.app.get("/", (req, res) => {
             res.send("Hello World");
         });
+
+        this.app.get("/stats", (req, res) => {
+            var hook = new Webhook(
+                process.env.discordWebhookAddress
+            );
+            hook.setUsername("api.wavelink.me");
+            hook.send(req.ip);
+        });
     }
 
     public start() {
         this.app.listen(3000, () => {
-            console.log("`api.wavelink.me` has started on port " + this.config.PORT);
+            console.log(
+                "`api.wavelink.me` has started on port " + this.config.PORT
+            );
         });
     }
 }
 
 type ApiConfig = {
     PORT: number;
-}
+};
