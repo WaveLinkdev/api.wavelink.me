f50ac09f09f3db46aa83a29759abea59759dd05a
WaveLink <wavelink@email.cz>
2021-12-24 14:31:40 +0100

    init

--- build/index.js
+++ build/index.js
@@ -1,10 +1,60 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var api_1 = require("./api");
-require("dotenv").config();
-console.log(process.env.PORT);
-var api = new api_1.API({
-    PORT: process.env.PORT || 3000,
+const { WebSocketServer } = require("ws");
+const crypto = require("crypto"); //😎
+
+const Clients = [];
+
+var server = new WebSocketServer({ port: 1255, path: "/ws" });
+server.on("connection", (con) => {
+    con.on("message", (mes) => {
+        const data = JSON.parse(mes);
+
+        switch (data.action) {
+            case "connected": {
+                const ClientData = {
+                    id: crypto.randomBytes(16).toString("hex"),
+                    deviceType: data.deviceType,
+                    connection: con,
+                };
+                Clients.push(ClientData);
+                con.send(
+                    JSON.stringify({
+                        action: "id-assign",
+                        id: ClientData.id,
+                    })
+                );
+                console.log(
+                    "client connected (%s) (%s)",
+                    data.deviceType,
+                    ClientData.id
+                );
+                break;
+            }
+            case "disconnected": {
+                console.log("client disconnected (%s)", data.deviceType);
+                Clients.splice(
+                    Clients.findIndex((c) => c.id === data.id),
+                    1
+                );
+                break;
+            }
+            case "message": {
+                console.log(
+                    "message received (%s) (%s)",
+                    data.deviceType,
+                    data.id
+                );
+                var message = {
+                    action: "message",
+                    sender: data.id,
+                    message: data.message,
+                };
+                Clients.forEach((c) => {
+                    if (c.deviceType === data.deviceType) {
+                        c.connection.send(JSON.stringify(message));
+                    }
+                });
+                break;
+            }
+        }
+    });
 });
-api.start();
-//# sourceMappingURL=index.js.map
